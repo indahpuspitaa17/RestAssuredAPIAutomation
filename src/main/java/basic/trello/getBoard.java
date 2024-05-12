0d55824c998d0319f34035933fa290918607cda0
Indah Puspita <indah.p@ninjavan.co>
2024-05-12 23:00:38 +0700

    remove secret

--- src/main/java/basic/trello/getBoard.java
+++ src/main/java/basic/trello/getBoard.java
@@ -19,12 +19,12 @@ public class getBoard {
 
         given().
         when().
-                get("/1/boards/io").
+                get("/1/boards/daGPAl4P").
         // Verify response and body
         then().
                 assertThat().statusCode(200).and().
                 contentType(ContentType.JSON).and().
-                body("id", equalTo("ii")).and().
+                body("id", equalTo("66409f6da0eedc2e6becfaa4")).and().
                 body("name", equalTo("Rest Assured"));
     }
 }
